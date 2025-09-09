f18f6367e5db1ba474f38ff5fa318390f63299cc
Kamalesh Marimuthu <kamaleshnj259@gmail.com>
2025-09-09 11:52:08 +0530

    Prime number checker
    Program to find the prime number from the input given.

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+//Prime number checker
+
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner ob4 = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = ob4.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
